ae3feb7287b3ebb9c58dda2b83ae873eddc5e516
Eric Lee <me@ericchanlee.com>
2026-01-22 14:32:45 -0800

    feat(nix): use opam-nix to build GLML, adding sandboxed opencode (probably will not use)

--- flake.nix
+++ flake.nix
@@ -1,19 +1,109 @@
 {
-  description = "GLML Compiler and Playground";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default =
-        pkgs.mkShell {
-          packages = with pkgs; [ glsl_analyzer ];
-        };
-    };
-}
+  description = "GLML nix flake";
 
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+    jail-nix.url = "sourcehut:~alexdavid/jail.nix";
+    llm-agents.url = "github:numtide/llm-agents.nix";
+  };
+
+  outputs = { self, flake-utils, opam-nix, nixpkgs, jail-nix, llm-agents }@inputs:
+    let
+      # Uses <package>.opam to solve dependencies from
+      package = "GLML";
+
+      # Opam packages used in developer mode
+      devOpamPackagesQuery = {
+        utop = "*";
+        ocaml-lsp-server = "*";
+        ocamlformat = "*";
+        merlin = "*";
+      };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+
+        # OCaml version can be specified here
+        opamPackagesQuery = devOpamPackagesQuery // {
+          ocaml-base-compiler = "*";
+        };
+
+        # OCaml Project Scope
+        scope = on.buildDuneProject { } package ./. opamPackagesQuery;
+
+        # Prevent the ocaml dependencies from leaking into dependent environments
+        overlay = final: prev: {
+          ${package} = prev.${package}.overrideAttrs (_: {
+            doNixSupport = false;
+          });
+        };
+        scope' = scope.overrideScope overlay;
+
+        # Expose OCaml packages defined in [devOpamPackagesQuery] to devshell
+        devOpamPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devOpamPackagesQuery) scope');
+        main = scope'.${package};
+
+        # Expose OCaml packages from opam derivation to opencode
+        opamPackages = builtins.filter pkgs.lib.isDerivation (builtins.attrValues scope');
+        ocamlPath = pkgs.lib.makeSearchPath "lib/ocaml/${scope'.ocaml-base-compiler.version}/site-lib" opamPackages;
+
+        opencode-pkg = llm-agents.packages.${system}.opencode;
+        jail = jail-nix.lib.init pkgs;
+
+        # Sandboxing for Opencode using bubblewrap
+        makeJailedOpencode = { extraPkgs ? [] } :
+          jail "jailed-opencode" opencode-pkg (with jail.combinators; [
+            network
+            time-zone
+            no-new-session
+
+            # Automatically grants access to your project source
+            mount-cwd
+
+            # Persistent config/cache for the agent
+            (readwrite (noescape "~/.config/opencode"))
+            (readwrite (noescape "~/.local/share/opencode"))
+            (readonly "/nix/store")
+
+            (set-env "OCAMLPATH" ocamlPath)
+
+            # Inject OCaml tools and common utilities that opencode can use
+            (add-pkg-deps (with pkgs; extraPkgs ++ ocamlPkgs ++ [
+                bashInteractive curl wget jq which
+                ripgrep gnugrep gawkInteractive ps findutils
+                diffutils binutils gcc
+            ]))
+          ]);
+
+      in
+      {
+        legacyPackages = scope';
+        packages.default = main;
+
+        devShells = {
+          # Shell with opam packages
+          default = pkgs.mkShell {
+            inputsFrom = [ main ];
+            packages = [
+              devOpamPackages
+              pkgs.glsl_analyzer
+            ];
+          };
+
+          # Shell with jailed opencode agent, nix develop ".#opencode"
+          opencode = pkgs.mkShell {
+            inputsFrom = [ main ];
+            packages = [
+              devOpamPackages
+              pkgs.glsl_analyzer
+              (makeJailedOpencode {})
+            ];
+          };
+        };
+      }
+    );
+}
