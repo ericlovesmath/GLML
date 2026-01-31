3e3c6cf7fb8f7737d56a3750dcf83f29a03caad8
Eric Lee <me@ericchanlee.com>
2026-01-30 16:34:33 -0800

    chore: improve js code

--- web/main.js
+++ web/main.js
@@ -1,7 +1,16 @@
+// Provides: state
+const state = {
+  gl: null,
+  program: null,
+  mouseX: 0,
+  mouseY: 0,
+  lastTime: 0,
+};
+
+// Provides: vs_source const
+//
 // Procedural full-screen triangle trick, covers whole square
 // Vertex 0: (-1, -1), Vertex 1: (3, -1), Vertex 2: (-1, 3)
-
-//Provides: vs_source
 const vs_source = `#version 300 es
   void main() {
     float x = -1.0 + float((gl_VertexID & 1) << 2);
@@ -9,14 +18,14 @@ const vs_source = `#version 300 es
     gl_Position = vec4(x, y, 0, 1);
   }`;
 
-// FPS Capping variables
-const fpsLimit = 120;
-const interval = 1000 / fpsLimit;
+// Provides: interval const
+const interval = 1000 / 120;
 
-//Provides: compileAndLinkGLSL
-//Requires: vs_source, render
+// Provides: compileAndLinkGLSL
+// Requires: vs_source, render, state
 function compileAndLinkGLSL(shader) {
-  let gl = window.gl;
+  const gl = state.gl;
+
   const compile = (source, type) => {
     let s = gl.createShader(type);
     gl.shaderSource(s, source);
@@ -31,86 +40,79 @@ function compileAndLinkGLSL(shader) {
   let fs;
   try {
     fs = compile(shader, gl.FRAGMENT_SHADER);
-  } catch (_) {
-    window.program = null;
+  } catch (error) {
+    console.error("GLSL Error!", error);
+    state.program = null;
     return null;
   }
 
-  window.program = gl.createProgram();
-  gl.attachShader(window.program, vs);
-  gl.attachShader(window.program, fs);
-  gl.linkProgram(window.program);
-  if (!gl.getProgramParameter(window.program, gl.LINK_STATUS))
-    throw Error(gl.getProgramInfoLog(window.program));
+  state.program = gl.createProgram();
+  gl.attachShader(state.program, vs);
+  gl.attachShader(state.program, fs);
+  gl.linkProgram(state.program);
+  if (!gl.getProgramParameter(state.program, gl.LINK_STATUS))
+    throw Error(gl.getProgramInfoLog(state.program));
 }
 
-//Provides: render
+// Provides: render
+// Requires: state, interval
 function render(currentTime) {
-  let gl = window.gl;
-  requestAnimationFrame(render);
+  let gl = state.gl;
 
-  const delta = currentTime - window.lastTime;
+  window.requestAnimationFrame(render);
 
-  if (delta >= interval && window.program != null) {
-    window.lastTime = currentTime - (delta % interval);
+  const delta = currentTime - state.lastTime;
+
+  if (delta >= interval && state.program != null) {
+    state.lastTime = currentTime - (delta % interval);
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.useProgram(window.program);
+    gl.useProgram(state.program);
     gl.uniform2f(
-      gl.getUniformLocation(window.program, "u_resolution"),
+      gl.getUniformLocation(state.program, "u_resolution"),
       gl.canvas.width,
       gl.canvas.height,
     );
     gl.uniform2f(
-      gl.getUniformLocation(window.program, "u_mouse"),
-      window.mouseX,
-      window.mouseY,
+      gl.getUniformLocation(state.program, "u_mouse"),
+      state.mouseX,
+      state.mouseY,
     );
-    gl.uniform1f(gl.getUniformLocation(window.program, "u_time"), currentTime);
+    gl.uniform1f(gl.getUniformLocation(state.program, "u_time"), currentTime);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
-    requestAnimationFrame(render);
+
+    window.requestAnimationFrame(render);
   }
-};
+}
 
-//Provides: init
-//Requires: vs_source, compileAndLinkGLSL, render
+// Provides: init
+// Requires: vs_source, compileAndLinkGLSL, render, state
 function init() {
-  window.gl = null;
-  window.program;
-  window.mouseX = 0;
-  window.mouseY = 0;
-  window.lastTime = 0;
-
   const canvas = document.getElementById("gl-canvas");
   const container = canvas.parentElement;
-  window.gl = canvas.getContext("webgl2");
+  state.gl = canvas.getContext("webgl2");
 
   // Find the smallest dimension to maintain a square, resize canvas
-  const resize = () => {
-    const rect = container.getBoundingClientRect();
-    const size = Math.min(rect.width, rect.height);
-
-    canvas.width = size;
-    canvas.height = size;
-
-    canvas.style.width = size + "px";
-    canvas.style.height = size + "px";
-  };
-
-  // TODO: Make this react to container size, not window
-  window.addEventListener("resize", resize);
+  const resizeObserver = new window.ResizeObserver((entries) => {
+    for (let entry of entries) {
+      const size = Math.min(entry.contentRect.width, entry.contentRect.height);
+      canvas.width = size;
+      canvas.height = size;
+      canvas.style.width = size + "px";
+      canvas.style.height = size + "px";
+    }
+  });
+  resizeObserver.observe(container);
 
   // Update mouseX and mouseY to be coords on canvas
   canvas.addEventListener("mousemove", (e) => {
     const rect = canvas.getBoundingClientRect();
-    window.mouseX = e.clientX - rect.left;
-    window.mouseY = rect.height - (e.clientY - rect.top);
+    state.mouseX = e.clientX - rect.left;
+    state.mouseY = rect.height - (e.clientY - rect.top);
   });
 
-  compileAndLinkGLSL("");
-  resize();
-  requestAnimationFrame((time) => {
-    window.lastTime = time;
+  window.requestAnimationFrame((time) => {
+    state.lastTime = time;
     render(time);
   });
 }
