bf1a75d2052dd27ee92a915fc5f526ca359dff6d
Eric Lee <me@ericchanlee.com>
2026-03-11 14:13:07 -0700

    feat(typecheck): reimplement with HM + Closed Typeclass universe, mostly copied from [ericlovesmath/TAPL], no idea if it works but the old tests seem to pass

--- compiler/typecheck.mli
+++ compiler/typecheck.mli
@@ -6,7 +6,6 @@ type term_desc =
   | Float of float
   | Int of int
   | Bool of bool
-  (* TODO: Vec, Math, Lam all don't need to store the size/ty now *)
   | Vec of int * term list
   | Mat of int * int * term list
   | Lam of string * ty * term
@@ -27,8 +26,6 @@ and term =
   }
 [@@deriving sexp_of]
 
-(* TODO: After typechecking, does [Define] and [Let] even need to
-   include the annotated type, I think I can delete it *)
 type top_desc =
   | Define of recur * string * term
   | Extern of string
@@ -44,5 +41,4 @@ type top =
 
 type t = Program of top list [@@deriving sexp_of]
 
-(** Returns a Typed AST, returns [Error _] if types are not sound *)
 val typecheck : Stlc.t -> t Or_error.t
