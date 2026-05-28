af5a8eacbf4ae29eb7601eafd3bbbb16eeeef512
Eric Lee <me@ericchanlee.com>
2026-05-28 01:29:48 -0700

    feat(playground): update the examples

--- playground/src/examples.ts
+++ playground/src/examples.ts
@@ -1,23 +1,21 @@
-import checkerboard from "../../examples/checkerboard.glml?raw";
-import mandelbrot from "../../examples/mandelbrot.glml?raw";
-import mouseCircle from "../../examples/mouse_circle.glml?raw";
-import rainbow from "../../examples/rainbow.glml?raw";
-import raymarch from "../../examples/raymarch.glml?raw";
-import recursion from "../../examples/recursion.glml?raw";
-import warpedNoise from "../../examples/warped_noise.glml?raw";
 import flatSDFViewer from "../../examples/2d_sdf_variants.glml?raw";
-import raymarchedPlanet from "../../examples/planet.glml?raw";
+import mandelbrot from "../../examples/mandelbrot.glml?raw";
+import mengerSponge from "../../examples/menger_sponge.glml?raw";
+import raymarch from "../../examples/raymarch.glml?raw";
+import warpedNoise from "../../examples/warped_noise.glml?raw";
 import beaver from "../../examples/beaver.glml?raw";
+import raymarchedMaterials from "../../examples/materials.glml?raw";
+import raymarchedPlanet from "../../examples/planet.glml?raw";
+import truchet from "../../examples/truchet.glml?raw";
 
 export const EXAMPLES: [string, string][] = [
+  ["Mandelbrot", mandelbrot],
   ["Metallic Liquid", warpedNoise],
   ["Raymarched Planet", raymarchedPlanet],
-  ["Mandelbrot", mandelbrot],
   ["2D SDFs", flatSDFViewer],
   ["Beaver Mascot", beaver],
   ["Raymarching", raymarch],
-  ["Recursive Star", recursion],
-  ["Mouse Tracking", mouseCircle],
-  ["Pastel Rainbow", rainbow],
-  ["Checkerboard", checkerboard],
+  ["Menger Sponge", mengerSponge],
+  ["Raymarch 3D Materials", raymarchedMaterials],
+  ["Truchet Tiling", truchet],
 ];
